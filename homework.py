1279114392a6112bdc705a10176f0635dd42fe8f
Aleksandr Dymchenko <aleksandrdymchenko@SPB-HQ-NB-1277.local>
2023-09-22 15:38:03 +0300

    fix titles

--- homework.py
+++ homework.py
@@ -23,9 +23,9 @@ ENDPOINT = 'https://practicum.yandex.ru/api/user_api/homework_statuses/'
 HEADERS = {'Authorization': f'OAuth {PRACTICUM_TOKEN}'}
 
 HOMEWORK_VERDICTS = {
-    'approved': 'Работа проверена: ревьюеру всё понравилось. Ура!',
-    'reviewing': 'Работа взята на проверку ревьюером.',
-    'rejected': 'Работа проверена: у ревьюера есть замечания.'
+    'approved': 'The homework had been checked!',
+    'reviewing': 'The homework is checking now',
+    'rejected': 'You have to fix something'
 }
 
 
